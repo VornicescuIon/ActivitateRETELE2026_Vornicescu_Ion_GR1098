a5d488da39c79991645d14ccafd452e49d3bdee7
Vornicescu Ion <vornicescuion23@stud.ase.ro>
2026-03-19 11:31:54 +0200

    first

--- Seminar3/Singleton/src/ro/ase/cts/main/Main.java
+++ Seminar3/Singleton/src/ro/ase/cts/main/Main.java
@@ -3,7 +3,7 @@ package ro.ase.cts.main;
 import ro.ase.cts.clase.Hotel;
 
 public class Main {
-    static void main(String[] args) {
+    public static void main(String[] args) {
         Hotel hotel1 = Hotel.getInstance("Intercont", 7, 3);
         Hotel hotel2 = Hotel.getInstance("MegaHotel", 200, 100);
 
